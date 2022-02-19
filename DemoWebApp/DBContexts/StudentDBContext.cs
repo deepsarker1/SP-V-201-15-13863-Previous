6bb101fb5a884758b807c395173c7590d4b44d6a
Deep Sarker <deepsarker14@gmail.com>
2022-02-19 15:26:30 +0600

    Initial Implementation

---
+++ DemoWebApp/DBContexts/StudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoWebApp.Models;
+namespace DemoWebApp.DBContexts
+{
+    public class StudentDBContext:DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Student{ get; set; }
+    }
+}
---
+++ DemoWebApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoWebApp.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string University { get; set; }
+
+    }
+}
